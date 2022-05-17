ca3f97aafa724f12ac8946e87e035217c7443498
Andrej Stevkovski <stevandrej@gmail.com>
2022-05-17 02:23:17 +0200

    Update getImageInfo.js

--- src/pages/SingleView/api/getImageInfo.js
+++ src/pages/SingleView/api/getImageInfo.js
@@ -1,3 +1,5 @@
+import { baseUrl } from 'config/baseUrl';
+
 export const getImageInfo = (id) => {
-	return fetch(`${process.env.REACT_APP_BASEURL}/id/${id}/info`);
+	return fetch(`${baseUrl}/id/${id}/info`);
 };
