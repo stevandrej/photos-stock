9435b73b2880ced5973db278f39754c54eaaf3a8
Andrej Stevkovski <stevandrej@gmail.com>
2022-05-17 20:02:57 +0200

    Album page

---
+++ src/pages/Album/Album.jsx
@@ -0,0 +1,15 @@
+import Container from 'components/Layout/Container/Container';
+import Section from 'components/Layout/Section/Section';
+import Row from 'components/Layout/Row/Row';
+
+const Album = () => {
+	return (
+		<Container alignMiddle>
+			<Section>
+				<Row>Album Page</Row>
+			</Section>
+		</Container>
+	);
+};
+
+export default Album;
