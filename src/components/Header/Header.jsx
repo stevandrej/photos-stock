fb4e9bd55798ff33fbc4d9421ff14e127c12b55b
Andrej Stevkovski <stevandrej@gmail.com>
2022-05-21 09:55:30 +0200

    UI and dropdown albums

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,14 @@
 import Button from 'components/Button/Button';
 import Container from 'components/Layout/Container/Container';
-import { useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { checkLogin } from 'utils/checkLogin';
 import { formatClasses } from 'utils/formatClasses';
 import styles from './Header.module.css';
 
 const Header = () => {
+	const navigate = useNavigate();
 	const albums = useSelector((state) => state.library.albums);
 	const [dropDownOpen, setDropDownOpen] = useState(false);
 
@@ -19,11 +21,35 @@ const Header = () => {
 		setDropDownOpen(!dropDownOpen);
 	};
 
+	const handleAlbumOpen = useCallback(
+		(id) => {
+			navigate(`/album/${id}`);
+		},
+		[navigate]
+	);
+
+	const renderDropDown = useMemo(() => {
+		return albums.length !== 0 ? (
+			albums.map((album) => (
+				<li
+					key={`dropdown-${album.id}`}
+					onClick={() => {
+						handleAlbumOpen(album.id);
+					}}
+				>
+					{album.name}
+				</li>
+			))
+		) : (
+			<li>Album list is empty</li>
+		);
+	}, [albums, handleAlbumOpen]);
+
 	return (
 		<header className={styles.header}>
 			<Container>
 				<div className={styles.headerContent}>
-					<div className={styles.headerTitle}>valstock</div>
+					<div className={styles.headerTitle}>Photos stock</div>
 
 					{checkLogin() && (
 						<div className={styles.headerButton}>
@@ -36,11 +62,7 @@ const Header = () => {
 									My Album
 								</Button>
 								<ul className={dropdownClasses}>
-									{albums.map((album) => (
-										<li key={`dropdown-${album.id}`}>
-											{album.name}
-										</li>
-									))}
+									{renderDropDown}
 								</ul>
 							</div>
 						</div>
